a4612b8604d240df775289b599a385b4760ff401
guru kumar <guru kumar>
2016-12-29 12:41:10 +0530

    getting userinput data

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,7 +12,10 @@ class Login extends CI_Controller {
 	  $this->form_validation->set_rules('password', 'Password', 'required');
 		if ($this->form_validation->run())
 		{
-		$this->load->view("admin_login");
+	//	$this->load->view("admin_login");
+		$username = $this->input->post('username');
+    $password = $this->input->post('password');
+		echo "username: $username and password: $password";
 		}
 		else
 		{
